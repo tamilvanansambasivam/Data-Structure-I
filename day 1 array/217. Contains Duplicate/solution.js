118ed809455417e2c1b0157d799362a7083cdb1a
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-12 17:03:14 +0530

    217. Contains Duplicate

---
+++ day 1 array/217. Contains Duplicate/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    let duplicates = []
+    
+    const tempArray = nums.sort()
+    
+    for (let i = 0; i < tempArray.length; i++) {
+      if (tempArray[i + 1] === tempArray[i]) {
+        duplicates.push(tempArray[i])
+      }
+    }
+    if (duplicates.length!=0){
+        return true
+    }
+    else{
+        return false
+    }
+    };
